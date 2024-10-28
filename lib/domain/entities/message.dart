58277e534db794d4009d87743ea90ed5dfaa381b
Gustavo <grafaelcarrillo@gmail.com>
2024-10-28 12:55:48 -0600

    Empezando ChatProvider

---
+++ lib/domain/entities/message.dart
@@ -0,0 +1,9 @@
+//identidad atómica: la unidad más pequeña de un sistema.
+enum FromWho{me, hers}
+class Message {
+  final String text;
+  final String? imageUrl;
+  final FromWho fromwho;
+
+  Message({required this.text, this.imageUrl, required this.fromwho});
+}
